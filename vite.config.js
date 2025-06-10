1ba6543048c5449132eefe3de1a4ea34e194a514
dazzycodes <dasolabello244@gmail.com>
2025-06-10 20:30:49 +0100

    Set up GitHub Pages deploy
    fix base path for deploy.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     resolve: {
         alias: {
           src: "/src",
-           base: '/peSport-eSport/', 
+           base: '/peSport-eSport/',
+            
         },
       },
     optimizeDeps: {
